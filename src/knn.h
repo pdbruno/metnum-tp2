af01194137f18b223df35852de60ebaa023ebcc4
pdbruno <pdbruno@gmail.com>
2021-05-11 19:00:02 -0300

    primer commit

---
+++ src/knn.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "types.h"
+
+
+class KNNClassifier {
+public:
+    KNNClassifier(unsigned int n_neighbors);
+
+    void fit(Matrix X, Matrix y);
+
+    Vector predict(Matrix X);
+private:
+};
