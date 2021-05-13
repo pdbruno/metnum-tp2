ebda93d350b56813f1c56cf196a4d8ed05864627
pdbruno <pdbruno@gmail.com>
2021-05-13 20:06:18 -0300

    primera implementacion de power iteration

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -6,19 +6,20 @@
 using namespace std;
 
 
-pair<double, Vector> power_iteration(const Matrix& X, unsigned num_iter, double eps)
-{
+pair<double, Vector> power_iteration(const Matrix &X, unsigned num_iter, double eps) {
     Vector b = Vector::Random(X.cols());
     double eigenvalue;
-    /***********************
-     * COMPLETAR CODIGO
-     **********************/
 
-    return make_pair(eigenvalue, b / b.norm());
+    for (int i = 0; i < num_iter; i++) {
+        b = X * b;
+        b = b / b.norm();
+    }
+
+    double norm = b.norm();
+    return make_pair(b.transpose() * A * b, b / norm);
 }
 
-pair<Vector, Matrix> get_first_eigenvalues(const Matrix& X, unsigned num, unsigned num_iter, double epsilon)
-{
+pair<Vector, Matrix> get_first_eigenvalues(const Matrix &X, unsigned num, unsigned num_iter, double epsilon) {
     Matrix A(X);
     Vector eigvalues(num);
     Matrix eigvectors(A.rows(), num);
