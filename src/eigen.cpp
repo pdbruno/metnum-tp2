05e7096a5f07768018d7ad08bf2f6391536853d3
msorondo <msorondo@live.com.ar>
2021-05-22 10:56:26 -0300

    test get_first_eigenvalues no pasa

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 
 pair<double, Vector> power_iteration(const Matrix &X, unsigned num_iter, double eps) {
-    Vector b = Vector::Random(X.cols());
+    Eigen::VectorXd b = Vector::Random(X.cols());
     bool very_close = false;
 
     for (int i = 0; i < num_iter && !very_close; i++) {
-        Vector new_b = X * b;
+        Eigen::VectorXd new_b = X * b;
         new_b = new_b / new_b.norm();
         double cos_angle = new_b.transpose() * b;
         very_close = (1-eps) < cos_angle && cos_angle <= 1;
@@ -25,11 +25,11 @@ pair<double, Vector> power_iteration(const Matrix &X, unsigned num_iter, double 
 pair<Vector, Matrix> get_first_eigenvalues(const Matrix &X, unsigned num, unsigned num_iter, double epsilon) {
     Matrix A;
     A = X;
-    Vector eigvalues(num);
-    Matrix eigvectors(A.rows(), num);
+    Eigen::VectorXd eigvalues(num);
+    Eigen::MatrixXd eigvectors(A.rows(), num);
 
     double a = 0;
-    Vector v = Vector::Zero(A.rows());
+    Eigen::VectorXd v = Vector::Zero(A.rows());
     for (int i = 0; i < num; i++) {
         A = A - (a * v * v.transpose());
         tie(a, v) = power_iteration(A, num_iter, epsilon);
