8ebcbf1ceed1f58bc75bab891298c2f2cb8fb340
pdbruno <pdbruno@gmail.com>
2021-05-25 12:44:25 -0300

    las olas y el viendoi sucundun sucundun

--- scripts/kfold_cross_val_metrics.py
+++ scripts/kfold_cross_val_metrics.py
@@ -2,31 +2,38 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import fetch_openml
 import numpy as np
-import pandas as pd
 import metnum
+from pathlib import Path
+import random
+
+best_ks = [2]
+kfold = [2]
 
 X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 y = y.astype(int)
 X = X.astype(int)
 
-limit = int(0.8 * X.shape[0]) 
+resultados = np.zeros((len(best_ks), len(kfold)))
 
-X_train, y_train = X[:limit], y[:limit]
-X_val, y_val = X[limit:], y[limit:]
+for index_k, k in enumerate(best_ks):
+    print(f'Current k: {k}')
+    for index_K, split in enumerate(kfold):
+        print(f'Current K: {split}')
+        kf = KFold(n_splits=split, shuffle=True)
+        accuracies_by_split = 0
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
 
-assert len(X_train) == len(y_train)
-assert len(X_val) == len(y_val)
-print(f"Ahora tengo {len(X_train)} instancias de entrenamiento y {len(X_val)} de validación")
+            clf_metnum = metnum.KNNClassifier(k)
+            clf_metnum.fit(X_train, y_train)
+            accuracies_by_split += accuracy_score(clf_metnum.predict(X_test), y_test)
 
-kf = KFold(n_splits=10)
-kf.get_n_splits(X)
-KFold(n_splits=10, random_state=None, shuffle=False)
-acc = 0
-for k in range(1, 101):
-    for train_index, test_index in kf.split(X):
-        clf_metnum = metnum.KNNClassifier(10)
-        clf_metnum.fit(X_train, y_train)
-        acc += accuracy_score(clf_metnum.predict(X_val), y_val)
+        resultados[index_k, index_K]= accuracies_by_split / split
 
-print(acc / 10)
+myfile = Path(f'kfold_cross_val_metrics/accuracies_by_k_by_K.npy')
+myfile.touch(exist_ok=True)
+with open(f'kfold_cross_val_metrics/accuracies_by_k_by_K.npy', 'wb') as f:
+    np.save(f, resultados)
+
 
