472d456b3353b577d1908d1662ca4d1263785a42
Martin Saied <martin@datia.app>
2021-05-27 17:03:00 -0300

    more things

--- scripts/graphics/heatmap.py
+++ scripts/graphics/heatmap.py
@@ -7,5 +7,5 @@ data = np.load('../k_and_alpha_heatmap/acc_heatmap_matrix.npy')
 print(data)
 df = pd.DataFrame(data, columns=[1,2,3,4,5,6,7,8,9,10,100,200,1000], index=[16,17,19,20,21,22,23,24])
 
-ax = sns.heatmap(df, vmin=0.95, vmax=1)
+ax = sns.heatmap(df, vmin=0.85, vmax=1)
 plt.show()
