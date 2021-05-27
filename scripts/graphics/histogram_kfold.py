09fe65210e2cb7d599edcb7335559f38db376bd5
pdbruno <pdbruno@gmail.com>
2021-05-27 19:05:10 -0300

    generacion de graficos kfold

--- scripts/graphics/histogram_kfold.py
+++ scripts/graphics/histogram_kfold.py
@@ -2,34 +2,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 best_ks = ['2','3','4','5','6']
+Kfolds = ['Sin K-Fold', '5', '10', '15', '20']
 accuracies = np.load('../kfold_cross_val_metrics/accuracies_by_k_by_K.npy')
 
-plt.rc('font', size=20)          # controls default text sizes
+""" 
 plt.rc('axes', titlesize=30)     # fontsize of the axes title
 plt.rc('axes', labelsize=30)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=30)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=30)    # fontsize of the tick labels
-plt.rc('legend', fontsize=15)   
+plt.rc('legend', fontsize=15)    """
 
-x = np.arange(len(best_ks))  # the label locations
-width = 0.13
-fig, ax = plt.subplots()
-accuracies_t = accuracies.T
-rects1 = ax.bar(x - 2 * width, [0.9445, 0.9455, 0.9455, 0.9445, 0.9455], width, label='Sin K-Fold')
-rects2 = ax.bar(x - width, accuracies_t[0], width, label='5')
-rects3 = ax.bar(x, accuracies_t[1], width, label='10')
-rects4 = ax.bar(x + width, accuracies_t[2], width, label='15')
-rects6 = ax.bar(x + 2 * width, accuracies_t[3], width, label='20')
+# function to add value labels
+def graficar(x,y, k):
+    plt.bar(x, y)
+    plt.ylabel('Accuracies')
+    plt.xlabel('Parametro k de kNN')
+    plt.title(f'Accuracies para k = {k} para distinto K de k-fold')
+    plt.ylim(0.8)
+    plt.rc('font', size=20)          # controls default text sizes
+    for i in range(len(x)):
+        plt.text(i, y[i], format(y[i], '.3f') , ha = 'center')
+    
+    plt.show()
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Accuracies')
-ax.set_xlabel('Parametro k de kNN')
-ax.set_title('Accuracies para k = 2, 3, 4, 5, 6 para distinto K de k-fold')
-ax.set_xticks(x)
-ax.set_ylim(0.7)
-ax.set_xticklabels(best_ks)
-ax.legend()
 
-fig.tight_layout()
 
-plt.show()
+accuracies_k_2 = accuracies[0].tolist()
+accuracies_k_3= accuracies[1].tolist()
+accuracies_k_4= accuracies[2].tolist()
+accuracies_k_5= accuracies[3].tolist()
+accuracies_k_6= accuracies[4].tolist()
+
+accuracies_k_2.insert(0, 0.9445)
+accuracies_k_3.insert(0, 0.9455)
+accuracies_k_4.insert(0, 0.9455)
+accuracies_k_5.insert(0, 0.9445)
+accuracies_k_6.insert(0, 0.9455)
+
+graficar(Kfolds, accuracies_k_3, 3)
+graficar(Kfolds, accuracies_k_4, 4)
+graficar(Kfolds, accuracies_k_5, 5)
+graficar(Kfolds, accuracies_k_6, 6)
